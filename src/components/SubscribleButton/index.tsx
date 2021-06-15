308fa31fe3f3b96abd44460e145b80ff04b6b297
JesseAraujo <jesse.araujo4@fatec.sp.gov.br>
2021-06-15 20:01:35 -0300

    home completed

---
+++ src/components/SubscribleButton/index.tsx
@@ -0,0 +1,14 @@
+import styles from './styles.module.scss'
+
+export function SubscribleButton() {
+    return (
+        <button
+            type='button'
+            className={styles.subscribleButton}
+        >
+            Subscrible now
+
+        </button>
+    )
+
+}
